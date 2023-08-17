5d9ae99d0444d9c6ee21bd01f7687a7b206ec831
Tan Jian Zhen <tanjianzhen3000@gmail.com>
2023-08-17 18:05:55 +0800

    feat: added simple why page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gray-900">
+  <div class="bg-gray-900 text-gray-100">
     <Title>Oolong</Title>
     <header class="absolute inset-x-0 top-0 z-50">
       <nav
@@ -7,10 +7,10 @@
         aria-label="Global"
       >
         <div class="flex lg:flex-1">
-          <a href="#" class="-m-1.5 p-1.5">
+          <NuxtLink to="/" class="-m-1.5 p-1.5">
             <span class="sr-only">Oolong</span>
             <img class="h-10 w-auto" src="/logo.png" alt="Oolong Logo" />
-          </a>
+          </NuxtLink>
         </div>
         <div class="flex lg:hidden">
           <button
@@ -23,14 +23,14 @@
           </button>
         </div>
         <div class="hidden lg:flex lg:gap-x-12">
-          <a
+          <NuxtLink
             v-for="item in navigation"
             :key="item.name"
-            :href="item.href"
+            :to="item.href"
             class="text-sm font-semibold leading-6 text-gray-100"
           >
             {{ item.name }}
-          </a>
+          </NuxtLink>
         </div>
         <div class="hidden lg:flex lg:flex-1 lg:justify-end"></div>
       </nav>
@@ -45,10 +45,10 @@
           class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
         >
           <div class="flex items-center justify-between">
-            <a href="#" class="-m-1.5 p-1.5">
+            <NuxtLink to="/" class="-m-1.5 p-1.5">
               <span class="sr-only">Oolong</span>
               <img class="h-10 w-auto" src="/logo.png" alt="Oolong Logo" />
-            </a>
+            </NuxtLink>
             <button
               type="button"
               class="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -61,13 +61,13 @@
           <div class="mt-6 flow-root">
             <div class="-my-6 divide-y divide-gray-500/10">
               <div class="space-y-2 py-6">
-                <a
+                <NuxtLink
                   v-for="item in navigation"
                   :key="item.name"
-                  :href="item.href"
+                  :to="item.href"
                   class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-100 hover:bg-gray-50"
-                  >{{ item.name }}</a
-                >
+                  >{{ item.name }}
+                </NuxtLink>
               </div>
               <div class="py-6"></div>
             </div>
@@ -76,46 +76,49 @@
       </Dialog>
     </header>
 
-    <div
-      class="h-screen w-screen relative overflow-hidden hidden md:block z-40"
-    >
-      <!-- <img
-        ref="eyes"
-        src="/images/eyes.gif"
-        alt="Eyes"
-        class="absolute w-screen inset-0"
-      /> -->
-      <video ref="eyes" autoplay loop muted playsinline>
-        <source src="/videos/eyes.webm" type="video/webm" />
-        <source src="/videos/eyes.mp4" type="video/mp4" />
-      </video>
-      <img
-        ref="charge"
-        src="/images/charge.gif"
-        alt="Charges out from Hero"
-        class="absolute inset-0"
-      />
-      <img
-        ref="hero"
-        src="/images/hero.png"
-        alt="Hero facing her demons"
-        class="absolute inset-0"
-      />
-    </div>
+    <!-- only show on landing -->
+    <section v-if="isLanding">
+      <div
+        class="h-screen w-screen relative overflow-hidden hidden md:block z-40"
+      >
+        <!-- <img
+          ref="eyes"
+          src="/images/eyes.gif"
+          alt="Eyes"
+          class="absolute w-screen inset-0"
+        /> -->
+        <video ref="eyes" autoplay loop muted playsinline>
+          <source src="/videos/eyes.webm" type="video/webm" />
+          <source src="/videos/eyes.mp4" type="video/mp4" />
+        </video>
+        <img
+          ref="charge"
+          src="/images/charge.gif"
+          alt="Charges out from Hero"
+          class="absolute inset-0"
+        />
+        <img
+          ref="hero"
+          src="/images/hero.png"
+          alt="Hero facing her demons"
+          class="absolute inset-0"
+        />
+      </div>
 
-    <div
-      class="h-screen w-screen relative overflow-hidden block md:hidden z-40"
-    >
-      <!-- <img
-        src="/images/hero-mobile.gif"
-        alt="Hero facing her demons"
-        class="absolute inset-0"
-      /> -->
-      <video autoplay loop muted playsinline>
-        <source src="/videos/hero-mobile.webm" type="video/webm" />
-        <source src="/videos/hero-mobile.mp4" type="video/mp4" />
-      </video>
-    </div>
+      <div
+        class="h-screen w-screen relative overflow-hidden block md:hidden z-40"
+      >
+        <!-- <img
+          src="/images/hero-mobile.gif"
+          alt="Hero facing her demons"
+          class="absolute inset-0"
+        /> -->
+        <video autoplay loop muted playsinline>
+          <source src="/videos/hero-mobile.webm" type="video/webm" />
+          <source src="/videos/hero-mobile.mp4" type="video/mp4" />
+        </video>
+      </div>
+    </section>
 
     <div class="relative isolate px-6 pt-14 lg:px-8">
       <div class="mx-auto max-w-4xl py-0 sm:py-16 lg:py-56">
@@ -145,7 +148,7 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/vue/24/outline";
 import { Dialog, DialogPanel } from "@headlessui/vue";
 
 const navigation = [
-  { name: "About", href: "#" },
+  { name: "Our Why", href: "/why" },
   { name: "Features", href: "#features" },
   { name: "Join our Beta", href: "#" },
 ];
@@ -158,6 +161,8 @@ let hero = ref(null);
 
 // close nav menu when route changes
 const route = useRoute();
+
+const isLanding = computed(() => route.path === "/");
 
 watch(
   route,
---
+++ pages/why.vue
@@ -0,0 +1,42 @@
+<template>
+  <article>
+    <h2 class="text-6xl font-bold tracking-widest text-center">Our Why</h2>
+    <hr class="mt-4" />
+
+    <!-- The Silent Crisis of Self-Worth -->
+    <section class="mt-6 grid grid-cols-1 md:grid-cols-2 items-center">
+      <div class="order-2">
+        <h3 class="text-2xl font-bold">The Silent Crisis of Self-Worth</h3>
+        <p class="mt-4">
+          In a world where one's value is often gauged by material success and
+          social media likes, a
+          <span class="font-bold underline">crisis of self-worth</span> is
+          looming large. A diminished sense of self-worth can lead to a myriad
+          of mental and emotional challenges.
+        </p>
+      </div>
+      <img
+        class="order-1 mt-4 md:mt-0"
+        src="/images/why/silent_crisis_of_self_worth.png"
+        alt="Staring intensely into the mirror"
+      />
+    </section>
+
+    <!-- A Path to Healing -->
+    <section class="mt-6 grid grid-cols-1">
+      <div class="md:order-1 order-2">
+        <h3 class="text-2xl font-bold mt-6">Volunteering: A Path to Healing</h3>
+        <p class="mt-4">
+          By volunteering, one not only aids the community but also re-discovers
+          their self-worth. The act of giving selflessly nurtures the soul,
+          fosters empathy, and builds a foundation of positive self-regard.
+        </p>
+      </div>
+      <img
+        class="mt-4 md:mt-0 md:order-2 order-1"
+        src="/images/why/a_path_to_healing.png"
+        alt="Staring intensely into the mirror"
+      />
+    </section>
+  </article>
+</template>
